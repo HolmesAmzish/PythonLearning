675d345f6944c87672f3d8f6b20307309a42523f
Nulla <holmesamzish86@outlook.com>
2024-09-13 17:33:10 +0800

    24-09-13

---
+++ WebDevelop/UDP-receiver.py
@@ -0,0 +1,18 @@
+"""
+@brief: client UDP socket program
+@date: 2024-09-13
+"""
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('', 8000))
+
+while True:
+    data, addr = s.recvfrom(1024)
+    data = data.decode()
+    print('received message:{0} from PORT {1[1]} on {1[0]}'.format(data, addr))
+    if data.lower() == 'bye':
+        break
+
+s.close()
---
+++ WebDevelop/UDP-sender.py
@@ -0,0 +1,11 @@
+"""
+@brief: UDP sender socket program
+@date: 2024-09-13
+"""
+
+import socket
+import sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(sys.argv[1].encode(), ('192.168.126.135', 8000))
+s.close()
